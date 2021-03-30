c2f9c3c4ef14f713daab8a3005dc9c611d215f48
MR-Geri <ilya.kamckine@ya.ru>
2021-03-30 22:46:27 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
-
+from kivy.storage.jsonstore import JsonStore
+from os.path import join
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -11,8 +12,6 @@ from kivy.uix.carousel import Carousel
 from kivy.uix.textinput import TextInput
 
 Window.size = (1080 // 2, 2240 // 2)
-# path = 'data'
-path = '/storage/emulated/0/'
 
 
 class Container(BoxLayout):
@@ -40,10 +39,66 @@ class MyTextInput(TextInput):
 
 
 class DiaryApp(App):
+    data_dir = App().user_data_dir
+
     def __init__(self):
         super(DiaryApp, self).__init__()
-        with open(f'{path}/data.json', encoding='utf-8') as file:
-            self.data = json.load(file)
+        try:
+            with open(f"{join(self.data_dir, 'data.json')}", encoding='utf-8') as file:
+                self.data = json.load(file)
+        except:
+            self.data = [
+                {'day': 'Понедельник',
+                 'lessons': [{'lesson': '', 'less_start': '8:00', 'less_finish': '8:40', 'task': '17 вариант'},
+                             {'lesson': 'общество', 'less_start': '8:50', 'less_finish': '9:30', 'task': ''},
+                             {'lesson': 'математика', 'less_start': '9:50', 'less_finish': '10:30',
+                              'task': '6.21-6.24'},
+                             {'lesson': 'физкулътура', 'less_start': '10:50', 'less_finish': '11:30',
+                              'task': ''},
+                             {'lesson': 'физика', 'less_start': '11:45', 'less_finish': '12:25', 'task': ''},
+                             {'lesson': 'икт', 'less_start': '12:40', 'less_finish': '13:20', 'task': ''},
+                             {'lesson': 'математика', 'less_start': '13:30', 'less_finish': '14:10',
+                              'task': ''}]}, {'day': 'Вторник', 'lessons': [
+                    {'lesson': 'математика', 'less_start': '8:00', 'less_finish': '8:40', 'task': '18-24 в,г'},
+                    {'lesson': 'математика', 'less_start': '8:50', 'less_finish': '9:30', 'task': ''},
+                    {'lesson': 'французский', 'less_start': '9:50', 'less_finish': '10:30', 'task': '151-152 '},
+                    {'lesson': 'физика', 'less_start': '10:50', 'less_finish': '11:30', 'task': '1.9-1.10'},
+                    {'lesson': 'русский язык', 'less_start': '11:45', 'less_finish': '12:25', 'task': '320'},
+                    {'lesson': 'литература', 'less_start': '12:40', 'less_finish': '13:20',
+                     'task': 'жизнь обман с чарующей тоской'},
+                    {'lesson': 'история', 'less_start': '13:30', 'less_finish': '14:10', 'task': ''},
+                    {'lesson': 'внеурочка русский', 'less_start': '14:20', 'less_finish': '15:00', 'task': '2.8'}]},
+                {'day': 'Среда',
+                 'lessons': [{'lesson': 'математика', 'less_start': '8:00', 'less_finish': '8:40', 'task': ''},
+                             {'lesson': 'физика', 'less_start': '8:50', 'less_finish': '9:30', 'task': ''},
+                             {'lesson': 'икт', 'less_start': '9:50', 'less_finish': '10:30', 'task': ''},
+                             {'lesson': 'родной язык', 'less_start': '10:50', 'less_finish': '11:30',
+                              'task': ''},
+                             {'lesson': 'литература', 'less_start': '11:45', 'less_finish': '12:25',
+                              'task': ''},
+                             {'lesson': 'астрономия', 'less_start': '12:40', 'less_finish': '13:20',
+                              'task': '17-22'},
+                             {'lesson': 'физкультура', 'less_start': '13:30', 'less_finish': '14:10',
+                              'task': ''}]}, {'day': 'Четверг', 'lessons': [
+                    {'lesson': 'французский', 'less_start': '8:00', 'less_finish': '8:40', 'task': ''},
+                    {'lesson': 'общество', 'less_start': '8:50', 'less_finish': '9:30', 'task': ''},
+                    {'lesson': 'математика', 'less_start': '9:50', 'less_finish': '10:30', 'task': '9.16, 9.20'},
+                    {'lesson': 'практикум', 'less_start': '10:50', 'less_finish': '11:30',
+                     'task': 'почему люди не понимают друг друга'},
+                    {'lesson': 'обж', 'less_start': '11:45', 'less_finish': '12:25', 'task': ''},
+                    {'lesson': 'физика', 'less_start': '12:40', 'less_finish': '13:20', 'task': '2.11-2.13'},
+                    {'lesson': 'математика', 'less_start': '13:30', 'less_finish': '14:10', 'task': ''}]},
+                {'day': 'Пятница', 'lessons': [
+                    {'lesson': 'французский', 'less_start': '8:00', 'less_finish': '8:40', 'task': ''},
+                    {'lesson': 'математика', 'less_start': '8:50', 'less_finish': '9:30',
+                     'task': '8-13 под б.'},
+                    {'lesson': 'физкулътура', 'less_start': '9:50', 'less_finish': '10:30', 'task': ''},
+                    {'lesson': 'литература', 'less_start': '10:50', 'less_finish': '11:30', 'task': ''},
+                    {'lesson': 'история', 'less_start': '11:45', 'less_finish': '12:25', 'task': ''},
+                    {'lesson': 'физика', 'less_start': '12:40', 'less_finish': '13:20', 'task': ''},
+                    {'lesson': 'икт', 'less_start': '13:30', 'less_finish': '14:10', 'task': ''},
+                    {'lesson': ' русский платные', 'less_start': '14:20', 'less_finish': '15:00',
+                     'task': '12 вар сенина'}]}]
         now = datetime.datetime.weekday(datetime.datetime.now())
         self.day = 0 if now == 5 or now == 6 else now
         self.start_of_the_week = datetime.date.today() - datetime.timedelta(days=datetime.date.today().weekday())
@@ -53,7 +108,7 @@ class DiaryApp(App):
 
     def save(self, instance, value):
         self.data[instance.day]['lessons'][instance.lesson]['task'] = value
-        with open(f'{path}/data.json', 'w') as f:
+        with open(f"{join(self.data_dir, 'data.json')}", 'w') as f:
             json.dump(self.data, f, indent=2)
 
     def build(self):
