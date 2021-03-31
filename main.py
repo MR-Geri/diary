a55586fa8f6df1c1c5c632756348603ba57d7491
ilyak <ilya.kamckine@ya.ru>
2021-03-31 09:56:32 +0300

    Новая версия

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import datetime
-from kivy.storage.jsonstore import JsonStore
+from kivy.uix.screenmanager import ScreenManager, Screen
 from os.path import join
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 import json
@@ -11,7 +12,7 @@ from kivy.core.window import Window
 from kivy.uix.carousel import Carousel
 from kivy.uix.textinput import TextInput
 
-Window.size = (1080 // 2, 2240 // 2)
+Window.size = (1080, 2240)
 
 
 class Container(BoxLayout):
@@ -30,19 +31,103 @@ class MyLabel(Label):
             Rectangle(pos=self.pos, size=self.size)
 
 
-class MyTextInput(TextInput):
+class MyButton(Button):
     def __init__(self, day: int, lesson: int, **kwargs):
         super().__init__(**kwargs)
         self.day = day
         self.lesson = lesson
-        # self.data[i]["lessons"][j]['lesson']
+
+    def on_press(self, *args, **kwargs):
+        app.text_edit.day = self.day
+        app.text_edit.lesson = self.lesson
+        app.text_edit.upd()
+        app.main_screen.manager.current = 'text_edit'
+
+
+class ScreenTextEdit(Screen):
+    def __init__(self, data, **kwargs):
+        super().__init__(**kwargs)
+        self.data = data
+        self.day = 0
+        self.lesson = 0
+        layout = BoxLayout(size_hint=(1, 0.6), pos_hint={'top': 1}, orientation='vertical')
+        self.text = TextInput(
+            foreground_color=(1, 1, 1, 1),
+            background_color=(0.5, 0.5, 0.5, 1),
+            cursor_color=(1, 1, 1, 1),
+            text=f'{self.data[self.day]["lessons"][self.lesson]["task"]}',
+            font_size='20sp'
+        )
+        layout.add_widget(self.text)
+        layout.add_widget(Button(text='Сохранить', on_press=self.main_screen, size_hint=(1, 0.1)))
+        self.add_widget(layout)
+
+    def main_screen(self, *args):
+        self.data[self.day]["lessons"][self.lesson]["task"] = self.text.text
+        self.manager.current = 'main_screen'
+        with open(f"{join(app.data_dir, 'data.json')}", 'w') as f:
+            json.dump(self.data, f, indent=2)
+        app.main_screen.data = self.data
+        app.main_screen.buttons[self.day][self.lesson].text = self.text.text
+
+    def upd(self):
+        self.text.text = self.data[self.day]["lessons"][self.lesson]["task"]
+
+
+class ScreenMain(Screen):
+    def __init__(self, data, **kwargs):
+        super().__init__(**kwargs)
+        self.data = data
+        now = datetime.datetime.weekday(datetime.datetime.now())
+        self.day = 0 if now == 5 or now == 6 else now
+        self.start_of_the_week = datetime.date.today() - datetime.timedelta(days=datetime.date.today().weekday())
+        self.date = self.start_of_the_week + datetime.timedelta(days=self.day)
+        if self.date < datetime.date.today():
+            self.start_of_the_week += datetime.timedelta(days=7)
+        self.buttons = []
+        main_layout = Container()
+        self.carousel = Carousel(loop=True)
+        for i in range(5):
+            day_layout = Container()
+            # дата
+            date_box = Container(size_hint=(1, 0.15))
+            date = self.start_of_the_week + datetime.timedelta(days=i)
+            date_label = MyLabel(text=f'{self.data[i]["day"]} [{date}]',
+                                 bg_color=(0.23, 0.23, 0.23, 1))
+            date_box.add_widget(date_label)
+            day_layout.add_widget(date_box)
+            #
+            temp = []
+            for j in range(len(self.data[i]["lessons"])):
+                lesson = self.data[i]["lessons"][j]
+                color = (0.23, 0.23, 0.23, 1) if j % 2 else (0.5, 0.5, 0.5, 1)
+                card = GridLayout(rows=2)
+                title = GridLayout(cols=2, size_hint_y=0.25)
+                title.add_widget(MyLabel(text=f'{lesson["lesson"]}', bg_color=color))
+                title.add_widget(MyLabel(text=f'{lesson["less_start"]}-{lesson["less_finish"]}', bg_color=color))
+                card.add_widget(title)
+                btn = MyButton(background_color=color, text=f'{lesson["task"]}', day=i, lesson=j)
+                temp.append(btn)
+                card.add_widget(btn)
+                day_layout.add_widget(card)
+            self.buttons.append(temp)
+            self.carousel.add_widget(day_layout)
+        self.carousel.index = self.day
+        main_layout.add_widget(self.carousel)
+        self.add_widget(main_layout)
+
+    def text_edit(self, *args):
+        self.manager.current = 'text_edit'
+
+    def save(self, instance, value):
+        self.data[instance.day]['lessons'][instance.lesson]['task'] = value
 
 
 class DiaryApp(App):
     data_dir = App().user_data_dir
 
     def __init__(self):
-        super(DiaryApp, self).__init__()
+        super().__init__()
         try:
             with open(f"{join(self.data_dir, 'data.json')}", encoding='utf-8') as file:
                 self.data = json.load(file)
@@ -99,54 +184,18 @@ class DiaryApp(App):
                     {'lesson': 'икт', 'less_start': '13:30', 'less_finish': '14:10', 'task': ''},
                     {'lesson': ' русский платные', 'less_start': '14:20', 'less_finish': '15:00',
                      'task': '12 вар сенина'}]}]
-        now = datetime.datetime.weekday(datetime.datetime.now())
-        self.day = 0 if now == 5 or now == 6 else now
-        self.start_of_the_week = datetime.date.today() - datetime.timedelta(days=datetime.date.today().weekday())
-        self.date = self.start_of_the_week + datetime.timedelta(days=self.day)
-        if self.date < datetime.date.today():
-            self.start_of_the_week += datetime.timedelta(days=7)
+        self.main_screen = ScreenMain(name='main_screen', data=self.data)
+        self.text_edit = ScreenTextEdit(name='text_edit', data=self.data)
 
-    def save(self, instance, value):
-        self.data[instance.day]['lessons'][instance.lesson]['task'] = value
+    def save(self):
         with open(f"{join(self.data_dir, 'data.json')}", 'w') as f:
             json.dump(self.data, f, indent=2)
 
     def build(self):
-        main_layout = Container()
-        carousel = Carousel(loop=True)
-        for i in range(5):
-            day_layout = Container()
-            # дата
-            date_box = Container(size_hint=(1, 0.15))
-            date = self.start_of_the_week + datetime.timedelta(days=i)
-            date_label = MyLabel(text=f'{self.data[i]["day"]} [{date}]',
-                                 bg_color=(0.23, 0.23, 0.23, 1))
-            date_box.add_widget(date_label)
-            day_layout.add_widget(date_box)
-            #
-            for j in range(len(self.data[i]["lessons"])):
-                lesson = self.data[i]["lessons"][j]
-                color = (0.23, 0.23, 0.23, 1) if j % 2 else (0.5, 0.5, 0.5, 1)
-                card = GridLayout(rows=2)
-                title = GridLayout(cols=2, size_hint_y=0.25)
-                title.add_widget(MyLabel(text=f'{lesson["lesson"]}', bg_color=color))
-                title.add_widget(MyLabel(text=f'{lesson["less_start"]}-{lesson["less_finish"]}', bg_color=color))
-                card.add_widget(title)
-                text = MyTextInput(
-                    foreground_color=(1, 1, 1, 1),
-                    background_color=color,
-                    cursor_color=(1, 1, 1, 1),
-                    text=f'{lesson["task"]}',
-                    font_size='25sp',
-                    day=i, lesson=j
-                )
-                text.bind(text=self.save)
-                card.add_widget(text)
-                day_layout.add_widget(card)
-            carousel.add_widget(day_layout)
-        carousel.index = self.day
-        main_layout.add_widget(carousel)
-        return main_layout
+        sm = ScreenManager()
+        sm.add_widget(self.main_screen)
+        sm.add_widget(self.text_edit)
+        return sm
 
 
 if __name__ == '__main__':
